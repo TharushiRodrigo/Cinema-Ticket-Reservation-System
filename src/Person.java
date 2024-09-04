463bd697c6bde352e7aad0fb80227704ead8170a
TharushiRodrigo <tharushirid@gmail.com>
2024-09-04 20:02:40 +0530

    Create Person.java file

---
+++ src/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+    private String name;
+    private String surname;
+    private String email;
+
+    public Person(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+    //Getters and setter for the name, surname, and email attributes
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
